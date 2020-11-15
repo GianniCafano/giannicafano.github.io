43006e58a7a215139190785c224612753a8c44a2
Gianni Cafano <gcafano@gmail.com>
2020-11-15 11:04:30 +0000

    removed location var as it is never used. Causing build failure <GC>

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const Home = () => {
-    const location = useLocation();
-
     return (
         <main>
             <h1>Home</h1>
