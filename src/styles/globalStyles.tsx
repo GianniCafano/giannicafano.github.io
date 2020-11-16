f4f02d16585049c2d67024c115c7a3b128f9f34b
Gianni Cafano <gcafano@gmail.com>
2020-11-16 20:03:30 +0000

    add height 100% to html and body to try and resolve mobile viewport height issue <GC>

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -10,6 +10,8 @@ const GlobalStyles = createGlobalStyle`
 
     html, 
     body {
+        height: 100%;
+        width: 100%;
         margin: 0;
         padding: 0;
         font-family: 'Varela Round', sans-serif;
